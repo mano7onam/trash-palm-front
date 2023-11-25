651f261aaf9a9a31c83cacfc1be49a4f03df16c1
Zelikson Stanislav <szelikson@tango.me>
2023-11-25 10:43:54 +0200

    Create empty project

---
+++ clientApp/clientAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  clientAppApp.swift
+//  clientApp
+//
+//  Created by Stanislav Zelikson on 25/11/2023.
+//
+//
+
+import SwiftUI
+
+@main
+struct clientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
